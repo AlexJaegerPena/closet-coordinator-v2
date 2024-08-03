69c013744343c2e0c7cafc94260c54af195c95c6
AlexJaegerPena <166617483+AlexJaegerPena@users.noreply.github.com>
2024-08-03 13:02:52 +0200

    daisyui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
