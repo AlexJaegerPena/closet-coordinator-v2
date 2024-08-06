8205b8aaf3abdafcf0d7dea6fb2cce4d8d9dfa8d
AlexJaegerPena <166617483+AlexJaegerPena@users.noreply.github.com>
2024-08-07 00:43:37 +0200

    minor changes

--- client/src/components/Wardrobe.jsx
+++ client/src/components/Wardrobe.jsx
@@ -1,7 +1,6 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
+
 import { Link } from "react-router-dom";
 import { FaRandom } from "react-icons/fa";
 import { TbMessageChatbot } from "react-icons/tb";
